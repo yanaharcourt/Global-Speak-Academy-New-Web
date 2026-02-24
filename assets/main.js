511a8d46c40e26b3235a4ab05bfda0cecc23146e
Yana Harcourt <yana.harcourt@gmail.com>
2026-02-24 16:42:11 +0100

    commit

--- assets/main.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', () => {
-    const nav = document.querySelector('nav');
-    if (window.scrollY > 40) {
-        nav.classList.add('scrolled');
-    } else {
-        nav.classList.remove('scrolled');
-    }
-});
